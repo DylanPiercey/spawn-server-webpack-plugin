9a5fb46d11d7f409cde99403df6780a118261251
Dylan Piercey <dpiercey@ebay.com>
2020-09-11 15:33:44 -0700

    chore: update tests to use memfs and recompile

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,44 +1,76 @@
-import os from "os";
 import path from "path";
+import { EventEmitter } from "events";
+import { Volume } from "memfs";
+import pEvent from "p-event";
 import fetch from "node-fetch";
-import webpack, { Watching } from "webpack";
+import webpack from "webpack";
 import SpawnServerPlugin from "../";
 
-const TEMP_DIR = os.tmpdir();
-let watcher: Watching;
-let server: SpawnServerPlugin;
-
-beforeEach(() => {
-  return new Promise((resolve, reject) => {
-    server = new SpawnServerPlugin();
-    watcher = webpack({
-      mode: "development",
-      name: "Server",
-      target: "node",
-      watch: true,
-      externals: [/^[^./!]/],
-      entry: path.join(__dirname, "fixtures/server.js"),
-      plugins: [server],
-      output: {
-        libraryTarget: "commonjs2",
-        path: TEMP_DIR
-      }
-    }).watch({}, (err, stats) => {
-      err = err || (stats.hasErrors() && new Error(stats.toString("errors-only")));
-
-      if (err) {
-        reject(err);
-      } else {
-        server.once("listening", resolve);
-      }
-    });
-  });
-});
-
-afterEach(() => new Promise(resolve => watcher.close(resolve)));
-
 test("Spawn Server Plugin", async () => {
-  const res = await fetch("http://localhost:3000");
-  const text = await res.text();
-  expect(text).toEqual("hi");
+  const entry = "/index.js";
+  const vol = Volume.fromJSON({
+    [entry]: createServerBoilerplate("hi")
+  })
+  const server = createBundledServer(vol, entry);
+
+  try {
+    await pEvent(server, "listening");
+    expect(await (await fetch("http://localhost:3000")).text()).toEqual("hi");
+  
+    vol.writeFileSync(entry, createServerBoilerplate("updated"));
+    server.invalidate();
+    await pEvent(server, "listening");
+
+    expect(await (await fetch("http://localhost:3000")).text()).toEqual("updated");
+  } finally {
+    await server.close();
+  }
 });
+
+function createBundledServer(vol: ReturnType<typeof Volume.fromJSON>, entry: string) {
+  (vol as typeof vol & { join: typeof path.join }).join = path.join.bind(path);
+  const emitter = new EventEmitter();
+  const server = new SpawnServerPlugin();
+  const compiler = webpack({
+    entry,
+    watch: true,
+    target: "node",
+    plugins: [server],
+    mode: "development",
+    externals: [/^[^./!]/],
+    output: {
+      libraryTarget: "commonjs2",
+      path: "/"
+    }
+  })
+
+  compiler.inputFileSystem = vol as typeof compiler.inputFileSystem;
+  compiler.outputFileSystem = vol as unknown as typeof compiler.outputFileSystem;
+
+  const watcher = compiler.watch({}, (err, stats) => {
+    err = err || (stats.hasErrors() && new Error(stats.toString("errors-only")));
+
+    if (err) {
+      emitter.emit("error", err);
+    } else {
+      server.once("listening", () => emitter.emit("listening"));
+    }
+  });
+
+  return Object.assign(emitter, {
+    close() {
+      return new Promise(resolve => watcher.close(resolve));
+    },
+    invalidate() {
+      watcher.invalidate();
+    }
+  });
+}
+
+function createServerBoilerplate(msg: string) {
+  return `
+  require("http").createServer((req, res) => {
+    res.end(${JSON.stringify(msg)});
+  }).listen(3000);
+  `
+}
